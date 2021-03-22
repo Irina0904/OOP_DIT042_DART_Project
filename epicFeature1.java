dac770104135b1c555e55b97c3ea6c49ac08cd5e
irina <irinale@student.chalmers.se>
2021-03-22 10:50:19 +0100

    Testing github for vs code

--- epicFeature1.java
+++ epicFeature1.java
@@ -1,3 +1,16 @@
+import java.util.*;
 
 public class epicFeature1 {
+    void TestingGit(int n){
+
+        Stack<Integer> s = new Stack<>();
+        
+        while(n > 0){
+            s.push(n % 2);
+            n = n / 2;
+        }
+        while(!s.isEmpty()){
+            System.out.println(s.pop());
+        }
+    }
 }
